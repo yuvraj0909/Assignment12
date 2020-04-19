25371890dff3f75a34d7690b912105cb412af960
yuvraj0909 <35275208+yuvraj0909@users.noreply.github.com>
2020-04-19 23:36:28 +0530

    Create file3.cpp

---
+++ file3.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+#include <conio.h>
+
+void main() {
+  clrscr();
+  int a,b;
+  cout << "Enter 2 nos ";
+  cin>>a>>b;
+  
+  cout << "The sum is "<<a+b<<endl;
+  cout << "The multiplication is "<<a*b;
+  getch();
+}
